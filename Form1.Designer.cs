4dc416afdc72adcdde6fbbe9f97661ce920588a0
Bence <78733248+bence9987@users.noreply.github.com>
2022-02-24 21:11:07 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace FreeRobux
+{
+    partial class rickrollform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.rick = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.PictureBox();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.lyrcs = new System.Windows.Forms.Label();
+            this.debugszamlalo = new System.Windows.Forms.Label();
+            this.lyrcs2 = new System.Windows.Forms.Label();
+            this.lyrcs3 = new System.Windows.Forms.Label();
+            this.cimke = new System.Windows.Forms.Label();
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.rick)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.label1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // rick
+            // 
+            this.rick.Image = global::FreeRobux.Properties.Resources.rick;
+            this.rick.Location = new System.Drawing.Point(168, 156);
+            this.rick.Name = "rick";
+            this.rick.Size = new System.Drawing.Size(219, 156);
+            this.rick.TabIndex = 4;
+            this.rick.TabStop = false;
+            this.rick.MouseDown += new System.Windows.Forms.MouseEventHandler(this.rick_MouseDown);
+            this.rick.MouseUp += new System.Windows.Forms.MouseEventHandler(this.rick_MouseUp);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Image = global::FreeRobux.Properties.Resources.rick;
+            this.label1.Location = new System.Drawing.Point(393, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 156);
+            this.label1.TabIndex = 5;
+            this.label1.TabStop = false;
+            // 
+            // timer4
+            // 
+            this.timer4.Enabled = true;
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // lyrcs
+            // 
+            this.lyrcs.AutoSize = true;
+            this.lyrcs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lyrcs.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lyrcs.ForeColor = System.Drawing.Color.Black;
+            this.lyrcs.Location = new System.Drawing.Point(0, 0);
+            this.lyrcs.Name = "lyrcs";
+            this.lyrcs.Size = new System.Drawing.Size(0, 25);
+            this.lyrcs.TabIndex = 6;
+            // 
+            // debugszamlalo
+            // 
+            this.debugszamlalo.AutoSize = true;
+            this.debugszamlalo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.debugszamlalo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.debugszamlalo.ForeColor = System.Drawing.Color.Black;
+            this.debugszamlalo.Location = new System.Drawing.Point(28, 119);
+            this.debugszamlalo.Name = "debugszamlalo";
+            this.debugszamlalo.Size = new System.Drawing.Size(24, 25);
+            this.debugszamlalo.TabIndex = 1;
+            this.debugszamlalo.Text = "0";
+            this.debugszamlalo.Visible = false;
+            // 
+            // lyrcs2
+            // 
+            this.lyrcs2.AutoSize = true;
+            this.lyrcs2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lyrcs2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lyrcs2.ForeColor = System.Drawing.Color.Black;
+            this.lyrcs2.Location = new System.Drawing.Point(0, 25);
+            this.lyrcs2.Name = "lyrcs2";
+            this.lyrcs2.Size = new System.Drawing.Size(0, 25);
+            this.lyrcs2.TabIndex = 7;
+            // 
+            // lyrcs3
+            // 
+            this.lyrcs3.AutoSize = true;
+            this.lyrcs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lyrcs3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lyrcs3.ForeColor = System.Drawing.Color.Black;
+            this.lyrcs3.Location = new System.Drawing.Point(0, 50);
+            this.lyrcs3.Name = "lyrcs3";
+            this.lyrcs3.Size = new System.Drawing.Size(0, 25);
+            this.lyrcs3.TabIndex = 8;
+            // 
+            // cimke
+            // 
+            this.cimke.AutoSize = true;
+            this.cimke.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.cimke.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cimke.ForeColor = System.Drawing.Color.Black;
+            this.cimke.Location = new System.Drawing.Point(326, 578);
+            this.cimke.Name = "cimke";
+            this.cimke.Size = new System.Drawing.Size(31, 13);
+            this.cimke.TabIndex = 9;
+            this.cimke.Text = "haha";
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // rickrollform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.cimke);
+            this.Controls.Add(this.debugszamlalo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rick);
+            this.Controls.Add(this.lyrcs3);
+            this.Controls.Add(this.lyrcs2);
+            this.Controls.Add(this.lyrcs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "rickrollform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RICKROLL";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.rickrollform_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.rickrollform_FormClosed);
+            this.Load += new System.EventHandler(this.rickroll_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rick)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.label1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox rick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox label1;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Label lyrcs;
+        private System.Windows.Forms.Label debugszamlalo;
+        private System.Windows.Forms.Label lyrcs2;
+        private System.Windows.Forms.Label lyrcs3;
+        private System.Windows.Forms.Label cimke;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+//Bence
+//
+//  This program was made to rickroll your friends. Rename the program to anyithing. For exmp. : Freerobux, discordnitro, win11install
+//
+//  30.10.2020
+//
+
+namespace FreeRobux
+{
+    public partial class rickrollform : Form
+    {
+        public rickrollform()
+        {
+            InitializeComponent();
+
+        }
+        int rickx = 6;
+        int ricky = 6;
+        int labelx = 6;
+        int labely = 6;
+
+
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+#region egy
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+
+
+            rick.Top -= ricky; 
+            rick.Left -= rickx;
+
+
+            if (rick.Top < 0 || rick.Top + rick.Height > ClientSize.Height)
+            {
+
+                ricky = -ricky;
+            }
+
+            else if (rick.Left < 0 || rick.Left + rick.Width > ClientSize.Width)
+            {
+
+                rickx = -rickx;
+            
+
+            }
+
+            cimke.Location = new Point(Cursor.Position.X +10, Cursor.Position.Y +10);
+            lyrcs.Location = new Point(rick.Location.X + 0, rick.Location.Y + 160);
+            lyrcs2.Location = new Point(rick.Location.X + 0, rick.Location.Y + 160);
+            lyrcs3.Location = new Point(rick.Location.X + 0, rick.Location.Y + 160);
+
+        }
+   
+
+        private void rickroll_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.Lime;
+            TransparencyKey = Color.Lime;
+            System.IO.Stream rickroll = Properties.Resources.RickRoll;
+            System.Media.SoundPlayer hang = new System.Media.SoundPlayer(rickroll);
+            hang.Play();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rickrollform_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (rick.Visible == true)
+            {
+                e.Cancel = true;
+                System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            }
+        }
+
+        private void rickrollform_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void rick_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer2.Start();
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            Cursor.Position = new Point(rick.Location.X + 109, rick.Location.Y + 78);
+
+        }
+
+        private void rick_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer2.Stop();
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+
+            label1.Top -= labely;
+            label1.Left -= labelx;
+            label1.Location = new Point(label1.Location.X + 0, rick.Location.Y + 0);
+
+            if (label1.Left < 0 || label1.Left + label1.Width > ClientSize.Width)
+            {
+
+                labelx = -labelx;
+            }
+
+        }
+#endregion 
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            int ertek = Convert.ToInt32(debugszamlalo.Text) + 1;
+            debugszamlalo.Text = Convert.ToString(ertek);
+            if (debugszamlalo.Text == "2")
+            {
+                timer5.Start();
+            
+            }
+            else if (debugszamlalo.Text == "6")
+            {
+               
+            }
+            else if (debugszamlalo.Text == "19")
+            {
+           
+                lyrcs.Text = "We're no strangers to love.";
+            }
+            else if (debugszamlalo.Text == "23")
+            {
+                lyrcs.Text = "You know the rules and so do I.";
+            }
+            else if (debugszamlalo.Text == "27")
+            {
+                lyrcs.Text = "A full commitment's what I'm thinking of.";
+           
+            }
+            else if (debugszamlalo.Text == "31")
+            {
+                lyrcs.Text = "You wouldn't get this from any other guy.";
+            }
+            else if (debugszamlalo.Text == "31")
+            {
+               
+                lyrcs.Text = "You wouldn't get this from any other guy.";
+            }
+            else if (debugszamlalo.Text == "35")
+            {
+                lyrcs.Text = "I just wanna tell you how I'm feeling.";
+           
+            }
+            else if (debugszamlalo.Text == "41")
+            {
+                lyrcs.Text = "Gotta make you understand,";
+            }
+            else if (debugszamlalo.Text == "43")
+            {
+                lyrcs3.Text = "Never gonna give you up \r\nNever gonna let you down \r\nNever gonna run around and desert you \r\nNever gonna make you cry \r\nNever gonna say goodbye \r\nNever gonna tell a lie and hurt you.";
+            }
+            else if (debugszamlalo.Text == "61")
+            {
+                lyrcs.Text = "";
+                lyrcs2.Text = "";
+                lyrcs3.Text = "";
+                rick.Visible = false;
+                label1.Visible = false;
+                cimke.Visible = false;
+                System.IO.Stream rickroll = Properties.Resources.RickRoll;
+                System.Media.SoundPlayer hang = new System.Media.SoundPlayer(rickroll);
+                hang.Stop();
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                timer4.Stop();
+                timer5.Stop();
+                this.Close();
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+           
+            timer5.Stop();
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
